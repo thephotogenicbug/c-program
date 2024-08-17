32eef3b2229424b5f8e203ba4146c06479613140
thephotogenicbug <navinkumarachari@gmail.com>
2024-08-17 13:53:15 +0530

    enum

---
+++ Enumeration/enumeration.c
@@ -0,0 +1,24 @@
+// Enumeration or enum
+#include <stdio.h>
+
+enum Bool{No, Yes};
+
+int main(){
+
+  enum point {X = 10, Y};
+
+  enum Bool a;
+
+  enum point b;
+
+  a = Yes;
+
+  b = Y;
+
+  printf("\n a : %d",a);
+
+  printf("\n b : %d",b);
+
+ return 0;
+
+}
